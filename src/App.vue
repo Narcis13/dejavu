aca2a429a71bcafc4775ea37af1797f666a024b4
Narcis13 <Narcis75@gmail.com>
2023-07-24 19:34:17 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="q-pa-md">
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+
+    <q-banner inline-actions class="text-white bg-red">
+      You have lost connection to the internet. This app is offline.
+      <template v-slot:action>
+        <q-btn flat color="white" label="Turn ON Wifi" />
+      </template>
+    </q-banner>
+  </div>
+  
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
