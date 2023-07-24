36b1a9ca2285e317cf0482aa109852a192317960
Narcis13 <Narcis75@gmail.com>
2023-07-24 19:34:52 +0300

    testtttt

--- src/main.js
+++ src/main.js
@@ -18,5 +18,5 @@ myApp.use(Quasar, {
   plugins: {}, // import Quasar plugins and add here
 })
 
-// Assumes you have a <div id="app"></div> in your index.html
+// Assumes you have a <div id="app"></div> in your index.html...
 myApp.mount('#app')
